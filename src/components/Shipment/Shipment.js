45b74a8c265aa54c1f5975a00b204ae6c4f03b61
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-10-01 22:39:00 +0600

    update contextApi from any child Component

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CatergoryContext } from '../../App';
 
 const Shipment = () => {
+    const [category, setCategory] = useContext(CatergoryContext);
+
     return (
         <div>
             <h1>This is Shipment</h1>
+            <button onClick={ () => setCategory(category + 2)} >Increment from Category</button>
         </div>
     );
 };
