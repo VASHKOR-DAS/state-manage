fbdd3493b338bbeb8f1de5f91792fc3d87864120
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-10-01 21:06:13 +0600

    pass data by props in siblings

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = (props) => {
+
+    const {count, setCount} = props
+
+    return (
+        <div>
+            <h1>This is Header : {count}</h1>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+        </div>
+    );
+};
+
+export default Header;
